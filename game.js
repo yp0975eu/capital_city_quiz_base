445b6e6c4716f0708c7a44ef194153b052f41c26
brndn adms <hey@brandenadams.com>
2020-02-27 17:19:47 -0600

    finish lab part 3

--- game.js
+++ game.js
@@ -2,32 +2,63 @@ let randomCountryElement = document.querySelector('#random-country')
 let userAnswerElement = document.querySelector("#user-answer")
 let submitButton = document.querySelector("#submit-answer")
 let resultTextElement = document.querySelector('#result')
+let playAgain = document.querySelector('#play-again')
 
 // TODO finish the script to challenge the user about their knowledge of capital cities.
 // An array of country codes is provided in the countries.js file. 
 // Your browser treats all JavaScript files as one big file, o
 // organized in the order of the script tags so the countriesAndCodes array is available to this script.
 
-console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
 
+// select random country
+function getRandomCountry() {
+  return countriesAndCodes[Math.floor(Math.random() * Math.floor(countriesAndCodes.length))]
+}
 
-// TODO when the page loads, select an element at random from the countriesAndCodes array
+let targetCountry
 
-// TODO display the country's name in the randomCountryElement 
+function play() {
+  targetCountry = getRandomCountry()
+  randomCountryElement.innerText = targetCountry.name
+  resultTextElement.innerText = ''
+  userAnswerElement.value = ''
+}
+
+playAgain.addEventListener('click', function (event) {
+  play()
+})
+
+play()
+
 
 // TODO add a click event handler to the submitButton.  When the user clicks the button,
-//  * read the text from the userAnswerElement 
+submitButton.addEventListener('click', async function (event) {
+  //  * read the text from the userAnswerElement
+  let userAnswer = userAnswerElement.value
+  let apiAnswer = await getCapitalFromApi(targetCountry['alpha-2'])
+  let correct = userAnswer.toLowerCase() === apiAnswer.toLowerCase()
+  let message = `You guessed ${apiAnswer} ${correct ? 'correctly': 'incorrectly!'}`
+  resultTextElement.innerText = message
+})
+
+function processApiResponse(data) {
+  const payload = 1
+  const countryInformation = 0
+  if (data[payload] && data[payload][countryInformation] && data[payload][countryInformation].capitalCity) {
+    return data[payload][countryInformation].capitalCity
+  }
+  alert('Api response does not have a capital :(')
+}
+
 //  * Use fetch() to make a call to the World Bank API with the two-letter country code (from countriesAndCodes, example 'CN' or 'AF')
-//  * Verify no errors were encountered in the API call. If an error occurs, display an alert message. 
-//  * If the API call was successful, extract the capital city from the World Bank API response.
-//  * Compare it to the user's answer. 
-//      You can decide how correct you require the user to be. At the minimum, the user's answer should be the same
-//      as the World Bank data - make the comparison case insensitive.
-//      If you want to be more flexible, include and use a string similarity library such as https://github.com/hiddentao/fast-levenshtein 
-//  * Finally, display an appropriate message in the resultTextElement to tell the user if they are right or wrong. 
-//      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"
-
-
-// TODO finally, connect the play again button. Clear the user's answer, select a new random country, 
-// display the country's name, handle the user's guess. If you didn't use functions in the code you've 
-// already written, you should refactor your code to use functions to avoid writing very similar code twice. 
+function getCapitalFromApi(countryCode) {
+  const url = `http://api.worldbank.org/v2/country/${countryCode}?format=json`
+  return fetch(url).then(res => {
+    //  * Verify no errors were encountered in the API call. If an error occurs, display an alert message. 
+    if (!res.ok || !res.stats === 200) {
+      alert(res.statusText)
+      return
+    }
+    return res.json()
+  }).then(processApiResponse) //  * If the API call was successful, extract the capital city from the World Bank API response.
+}
